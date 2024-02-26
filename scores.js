a0950a77ddb4ec66b1bb1afa9d61fae93979a4fc
Chase Cook <chasefcook@gmail.com>
2024-02-26 14:41:46 -0700

    finish loadScores function

--- scores.js
+++ scores.js
@@ -13,8 +13,21 @@ function loadScores() {
             const nameTdEl = document.createElement('td');
             const timeTdEl= document.createElement('td');
             const dateTdEl = document.createElement('td');
+
+            positionTdEl.textConent = i + 1;
+            nameTdEl.textContent = time.name;
+            timeTdEl.textContent = time.score;
+            dateTdEl.textContent = time.date;
+
+            const rowEl = document.createElement('tr');
+            rowEl.appendChild(positionTdEl);
+            rowEl.appendChild(nameTdEl);
+            rowEl.appendChild(timeTdEl);
+            rowEl.appendChild(dateTdEl);
+
+            tableBodyEl.appendChild(rowEl);
         } 
+    }   else {
+        tableBodyEl.innerHTML = '<tr><td colSpan=4>Be the first to play</td></tr>';
     }
-
-
 }

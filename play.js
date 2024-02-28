d5ea5dfa64b7ef1f38267eecdce38d9c6b87849e
Chase Cook <chasefcook@gmail.com>
2024-02-28 12:40:33 -0700

    format time

--- play.js
+++ play.js
@@ -53,3 +53,12 @@ function resetTimer() {
     clearInterval(timerInterval);
     startTimer();
 }
+
+function formatTime(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+}
+
+
+    startTimer();

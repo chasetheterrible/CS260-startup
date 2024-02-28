1d2d8cd50506a9820e5f544064ebff6c1607983a
Chase Cook <chasefcook@gmail.com>
2024-02-27 20:07:05 -0700

    keep 1 grid

--- play.js
+++ play.js
@@ -1,7 +1,7 @@
 const cardValues= [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8]
 
 function shuffleArray(array) {
-    for (let i = arra.length - 1; i > 0; i -- ) {
+    for (let i = array.length - 1; i > 0; i -- ) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
@@ -10,10 +10,36 @@ function shuffleArray(array) {
 
 shuffleArray(cardValues);
 
-const buttonContainer = document.getElementsByClassName("button-container")
+const buttonContainers = document.getElementsByClassName("button-container")
 
-for (let i = 0; i < cardValues.length; i ++) {
-    const button = document.createElement("button");
-    button.textContent = cardValues[i];
-    buttonContainer.appendChild(button);
+let clickedButtons = [];
+
+function handleButtonClick(event) {
+    const button = event.target;
+    button.classList.toggle("clicked")
+    clickedButtons.push(button.textContent);
+    if (clickedButtons.length === 2) {
+        if (clickedButtons[0] === clickedButtons[1]) {
+            alert("Match found!");
+        } else {
+            setTimeout(() => {
+                for (const clickedButton of document.querySelectorAll(".clicked")) {
+                    clickedButton.classList.remove("clicked");
+                }
+            }, 1000);
+        }
+
+        clickedButtons = [];
+    }
+}
+
+
+for (const buttonContainer of buttonContainers) {
+    for (let i = 0; i < cardValues.length; i ++) {
+        const button = document.createElement("button");
+        button.textContent = cardValues[i];
+        buttonContainer.appendChild(button);
+        button.addEventListener("click", handleButtonClick)
+    }
+        
 }

8531b486048b58bd8f8c7aff5f2c9eee5e97d016
Chase Cook <chasefcook@gmail.com>
2024-02-28 19:25:14 -0700

    all done

--- scores.js
+++ scores.js
@@ -9,18 +9,15 @@ function loadTimes() {
 
     if (times.length) {
         for (const [i, time] of times.entries()) {
-            const positionTdEl= document.createElement('td');
             const nameTdEl = document.createElement('td');
             const timeTdEl= document.createElement('td');
             const dateTdEl = document.createElement('td');
 
-            positionTdEl.textConent = i + 1;
             nameTdEl.textContent = time.name;
             timeTdEl.textContent = time.time;
             dateTdEl.textContent = time.date;
 
             const rowEl = document.createElement('tr');
-            rowEl.appendChild(positionTdEl);
             rowEl.appendChild(nameTdEl);
             rowEl.appendChild(timeTdEl);
             rowEl.appendChild(dateTdEl);

81ce59d9cbb57be103a91712b8b3d473b7017eb9
Chase Cook <chasefcook@gmail.com>
2024-03-09 19:45:25 -0700

    desiplay image function

---
+++ Public/about.js
@@ -0,0 +1,19 @@
+function displayPicture() {
+    const random = Math.floor(Math.random() * 1000);
+    fetch(`https://picsum.photos/v2/list?page=${random}&limit=1`)
+        .then((response) => response.json())
+        .then((data) => {
+            const containerEl = document.querySelector('#picture');
+
+            const width = container.offsetWidth;
+            const height = contianerEl.offsetHeight;
+
+            const imgUrl = `https://picsum.photos/id/${data[0].id}/${width}/${height}?grayscale`;
+            const imgEl = document.createElement('img');
+            imgEl.setAttribute('src', imgUrl);
+            containerEl.appendChild(imgEl);
+        });
+} 
+function displayQuote(data) {
+    
+}

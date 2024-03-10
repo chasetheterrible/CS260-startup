c90b18a2148e68ef1930bd64c79abc45efe4354e
Chase Cook <chasefcook@gmail.com>
2024-03-09 19:59:22 -0700

    display quote function

--- Public/about.js
+++ Public/about.js
@@ -15,5 +15,20 @@ function displayPicture() {
         });
 } 
 function displayQuote(data) {
-    
+    fetch('https://dad-jokes.p.rapidapi.com/random/joke')
+    .then((response) => response.json)
+    .then((data) => {
+        const containerEl = documment.querySelector('#joke');
+
+        const jokeEl = document.createElement('p');
+        jokeEl.classList.add('joke');
+        const authorEl = document.createElement('p');
+        authorEl.classList.add('author');
+
+        jokeEl.textContent = data.content;
+        authorEl.textContent = data.author;
+
+        containerEl.appendChild(jokeEl)
+        containerEl.appendChild(authorEl);
+    })
 }

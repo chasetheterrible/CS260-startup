07e66d89bfbeb1bcb1af958a048b92d83eaf55fd
Chase Cook <chasefcook@gmail.com>
2024-03-10 21:55:21 -0600

    fixing some stuff

--- login.js
+++
@@ -1,6 +0,0 @@
-function login() {
-    const userNameEl = document.querySelector("#Username");
-    const passwordEl = document.querySelector("#password");
-    localStorage.setItem("userName", userNameEl.value);
-    localStorage.setitem("Password", passwordEl);
-}

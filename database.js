5496d24cdb917e2cd06fa39ffe3e7ba5aa90adc4
Chase Cook <chasefcook@gmail.com>
2024-03-20 13:58:52 -0600

    editing database.js

--- database.js
+++ database.js
@@ -1,2 +1,6 @@
+const { MogoClient } = require('mongodb');
+const bycrypt = require('bycrypt');
+const uuid = require('uuid');
 const config = require('./dbConfig.json');
+
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;

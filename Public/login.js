520e33fa672a0fe5d50ed1a7d2ab231193927eda
Chase Cook <chasefcook@gmail.com>
2024-03-19 23:28:52 -0600

    start login.js

--- Public/login.js
+++ Public/login.js
@@ -1,6 +1,15 @@
-function login() {
-    const userNameEl = document.querySelector("#Username");
-    // const passWordEl = document.querySelector("#password");
-    localStorage.setItem("userName", userNameEl.value);
-    // localStorage.setitem("password", passWordEl.value);
+(async () => {
+    const userName = localStorage.getItem('userName');
+    if(userName) {
+        document.querySelector('#playerName').textContent = userName;
+        setDisplay('loginControls', 'none');
+        setDisplay('playControls', 'block');
+    } else {
+        setDisplay('loginControls', 'block');
+        setDisplay('playControls', 'none');
+    }
+})();
+
+async function createUser() {
+    loginOrCreate(`/api/auth/create`);
 }

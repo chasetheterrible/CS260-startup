d5fab6a349684f18cac54c59608727367e1750ae
Chase Cook <chasefcook@gmail.com>
2024-03-19 23:09:36 -0600

    display joke data

--- Public/about.js
+++ Public/about.js
@@ -1,23 +1,11 @@
-function displayQuote(data) {
-    fetch('https://api.chucknorris.io/jokes/random?category=dev')
-    .then((response) => response.json())
-    .then((data) => {
-        const containerEl = document.querySelector('#joke');
+function displayJoke(data) {
+    const containerEl = document.querySelector('#joke');
 
-        const jokeEl = document.createElement('p');
-        jokeEl.classList.add('joke');
-        const authorEl = document.createElement('p');
-        authorEl.classList.add('author');
+    const jokeEl = document.createElement("p");
+    jokeEl.classList.add("joke");
+    
+    jokeEl.textContent = data.content;
 
-        jokeEl.textContent = data.value;
-
-        containerEl.appendChild(jokeEl)
-        containerEl.appendChild(authorEl);
-
-        console.log(data.value)
-        console.log(data)
-    });
+    containerEl.appendChile(jokeEl);
 }
-
-
-displayQuote();
+displayJoke();

4acddd8516634d78ac0723bc8dee11a8058d9043
Chase Cook <chasefcook@gmail.com>
2024-03-20 21:24:11 -0600

    fixing some minor errors in DB.js

--- database.js
+++ database.js
@@ -1,4 +1,4 @@
-const { MogoClient } = require('mongodb');
+const { MongoClient } = require('mongodb');
 const bcrypt = require('bcrypt');
 const uuid = require('uuid');
 const config = require('./dbConfig.json');

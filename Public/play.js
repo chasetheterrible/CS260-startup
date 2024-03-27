5ea6a69f19427f036eeed1da737b3f3762a740ad
Chase Cook <chasefcook@gmail.com>
2024-03-27 12:20:31 -0600

    play.js editing

--- Public/play.js
+++ Public/play.js
@@ -183,5 +183,26 @@ function updateTimesLocal(newTime) {
 
 configureWebSocket() {
     const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
-    this.socket = new WebSocket
+    this.socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
+    this.socket.onopen = (event)  => {
+        this.displayMsg('system', 'game', 'connected');
+    };
+    this.socket.onclose = (event) => {
+        this.displayMsg('system', 'game', 'disconnected');
+    };
+    this.socket.onmessage = async (event) => {
+        const msg = JSON.parse(await event.data.text());
+        if (msg.type === GameEndEvent) {
+            this.displayMsg('player', msg.from, `scored ${msg.value.time}`);
+        } else if (msg.type === GameStartEvent) {
+            this.displayMsg('player', msg.from, `started a new game`);
+        }
+    };
 }
+
+displayMsg(cls, form, msg) {
+    const chatText = document.querySelector('#player-messages');
+    chatText.innerHTML = `<div class="event"><span class="${cls}-event">${from}</span> ${msg}</div>` + chatText.innerHTML;
+}
+
+broadcastEvent(from, type, value)

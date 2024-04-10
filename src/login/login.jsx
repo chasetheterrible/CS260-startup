f63a7c4e0f9580705f0895a478171dc7ebd033eb
Chase Cook <chasefcook@gmail.com>
2024-04-10 11:15:25 -0600

    login.jsx tabbing

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -5,22 +5,22 @@ import { Authenticated } from './authenticated';
 import { AuthState } from './authState';
 
 export function Login({ userName, authState, onAuthChange }) {
-    return (
-        <main className='container-fluid bg-secondary text-center'>
-        <div>
-          {authState !== AuthState.Unknown && <h1>Paired Up</h1>}
-          {authState === AuthState.Authenticated && (
-            <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
-          )}
-          {authState === AuthState.Unauthenticated && (
-            <Unauthenticated
-              userName={userName}
-              onLogin={(loginUserName) => {
-                onAuthChange(loginUserName, AuthState.Authenticated);
-              }}
-            />
-          )}
-        </div>
-      </main>
+  return (
+      <main className='container-fluid bg-secondary text-center'>
+      <div>
+        {authState !== AuthState.Unknown && <h1>Paired Up</h1>}
+        {authState === AuthState.Authenticated && (
+          <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
+        )}
+        {authState === AuthState.Unauthenticated && (
+          <Unauthenticated
+            userName={userName}
+            onLogin={(loginUserName) => {
+              onAuthChange(loginUserName, AuthState.Authenticated);
+            }}
+          />
+        )}
+      </div>
+    </main>
     );
 }

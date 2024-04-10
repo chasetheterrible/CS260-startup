340f0a53bfac8d3df927ffc39aec99792eaa4937
Chase Cook <chasefcook@gmail.com>
2024-04-10 12:52:21 -0600

    play.jsx and game jsx creation

---
+++ src/play/play.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Players } from './players';
+import { PairedUpGame } from './pairedupGame';
+
+export function Play(props) {
+    return (
+        <main class="bg-secondary">
+          <Players userName={props.userName} />
+          <PairedUpGame userName={props.userName} />
+        </main>
+    );
+}

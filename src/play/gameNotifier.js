66df0b11a23e482280ac15b521ce26a8e3b588aa
Chase Cook <chasefcook@gmail.com>
2024-04-10 12:40:11 -0600

    gameNotifier.js

--- src/play/gameNotifier.js
+++ src/play/gameNotifier.js
@@ -15,12 +15,49 @@ class EventMessage {
 class GameEventNotifier {
     events = [];
     handlers = [];
-}
 
-    function constructor() {
-        let port = window.location.protocol === 'http:' ? 'ws' : 'wss';
+    constructor() {
+        let port = window.location.port;
+        const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
         this.socket = new WebSocket(`${protocol}://${window.location.hostname}:${port}/ws`);
         this.socket.onopen = (event) => {
             this.receiveEvent(new EventMessage('PairedUp', GameEvent.System, { msg: 'connected' }));
         };
+        this.socket.onclose = (event) => {
+            this.receiveEvent(new EventMessage('PairedUp', GameEvent.System, { msg: 'disconnected' }));
+        };
+        this.socket.onmessage = async (msg) => {
+            try {
+                const event = JSON.parse(await msg.data.text());
+                this.receiveEvent(event);
+            } catch {}
+        };
     }
+
+
+    broadcastEvent(from, type, value) {
+        const event = new EventMessage(from, type, value);
+        this.socket.send(JSON.stringify(event));
+    }
+
+    addHandler(handler) {
+        this.handlers.push(handler);
+    }
+
+    removeHandler(handler) {
+        this.handlers.filter((h) => h !== handler);
+    }
+
+    receiveEvent(event) {
+        this.events.push(event);
+
+        this.events.forEach((e) => {
+            this.handlers.forEach((handler) => {
+                handler(e);
+            });
+        });
+    }
+}
+
+const GameNotifier = new GameEventNotifier();
+export { GameEvent, GameNotifier};

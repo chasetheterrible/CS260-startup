b673139f78652cc3946bf90a20f1e4e60b49812f
Chase Cook <chasefcook@gmail.com>
2024-04-16 11:28:12 -0600

    scores -> times

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
 import { Login } from "./login/login";
 import { Play } from "./play/play";
-import { Scores } from "./scores/scores";
+import { Times } from "./times/times";
 import { About } from "./about/about";
 import { AuthState } from "./login/authState";
 import "bootstrap/dist/css/bootstrap.min.css";
